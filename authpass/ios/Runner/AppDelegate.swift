c1302e46218bcbe9af08716403283af686741a47
ozhalgas <56766998+ozhalgas@users.noreply.github.com>
2022-04-01 19:54:06 +0600

    Screenshot Disabling for iOS devices

--- authpass/ios/Runner/AppDelegate.swift
+++ authpass/ios/Runner/AppDelegate.swift
@@ -21,3 +21,21 @@ import Flutter
         return false
     }
 }
+
+@implementation AppDelegate
+- (BOOL)application:(UIApplication *)application
+    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  [GeneratedPluginRegistrant registerWithRegistry:self];
+  // Override point for customization after application launch.
+  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application{
+    self.window.hidden = YES;
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application{
+    self.window.hidden = NO;
+}
+
+@end
